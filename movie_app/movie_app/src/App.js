86c16f2790bb6c7c3f01f5ad00f1c16dc5ffc3ce
lyg0114 <whdnseowkd@naver.com>
2017-10-14 10:03:31 +0900

    #4-3. Loading States
    로딩 구현

--- movie_app/movie_app/src/App.js
+++ movie_app/movie_app/src/App.js
@@ -9,31 +9,7 @@ class App extends Component {
     greeting:'Hello!',
     testforstate:'stategggg',
     
-    movies: [ //컴포넌트 내부로 데이터 이동
-      
-        { 
-          title: "Matrix2" ,
-          poster: "http://cfile2.uf.tistory.com/image/03217F4A509D113526C558" 
-        },
-
-        { 
-          title: "Full Metal Jacket",
-          poster: "https://resizing.flixster.com/LTuNtJsmLevuXO2kS_Uh8xMUDa0=/206x305/v1.bTsxMTE2ODAyOTtqOzE3NTQ0OzEyMDA7ODAwOzEyMDA",//자켓
-        },
-
-        {
-          
-          title: "Oldboy" ,
-          poster: "https://upload.wikimedia.org/wikipedia/ko/thumb/4/48/Old_Boy.jpg/250px-Old_Boy.jpg" 
-        },
-
-        {
-          title: "Star Wars" ,
-          poster: "http://cfile29.uf.tistory.com/image/275C6D465613CFDD0D4D4A" 
-        }
-
-      ]
-
+    
   }
   
   componentDidMount(){
@@ -42,33 +18,47 @@ class App extends Component {
       
       this.setState({
         
-        movies:[
+        movies: [ //컴포넌트 내부로 데이터 이동
           
-          ...this.state.movies, //앞에 선언되어있었던 놈들~
+            { 
+              title: "Matrix2" ,
+              poster: "http://cfile2.uf.tistory.com/image/03217F4A509D113526C558" 
+            },
+    
+            { 
+              title: "Full Metal Jacket",
+              poster: "https://resizing.flixster.com/LTuNtJsmLevuXO2kS_Uh8xMUDa0=/206x305/v1.bTsxMTE2ODAyOTtqOzE3NTQ0OzEyMDA7ODAwOzEyMDA",//자켓
+            },
+    
+            {
+              
+              title: "Oldboy" ,
+              poster: "https://upload.wikimedia.org/wikipedia/ko/thumb/4/48/Old_Boy.jpg/250px-Old_Boy.jpg" 
+            },
+    
+            {
+              title: "Star Wars" ,
+              poster: "http://cfile29.uf.tistory.com/image/275C6D465613CFDD0D4D4A" 
+            }
+    
+          ]
+          })
 
-          {
-            title: "Transpotting",
-            poster: "https://images-na.ssl-images-amazon.com/images/M/MV5BMDlhMjIwN2UtZTE5ZC00YzQ2LWI3ODYtZTMzY2MyN2VmYTI4L2ltYWdlXkEyXkFqcGdeQXVyNDUzOTQ5MjY@._CR67,56,750,750_UX402_UY402._SY201_SX201_AL_.jpg"
-          },
-
-          
-
-        ]
-      })
-
-    },3000)
+    },1000)
     
   }
-  
+
+  _renderMovies = () =>{
+    const movies = this.state.movies.map((movie,index) =>{ 
+      return <Movie title={movie.title} poster={movie.poster} key={index} />
+    })
+    return movies
+  }
+
   render() { 
     return ( 
-      
       <div className="App">
-        <br/>
-            { this.state.movies.map((movie,index) =>{ 
-              return <Movie title={movie.title} poster={movie.poster} key={index} />
-            })}
-        
+        {this.state.movies ? this._renderMovies() : 'Loading' }
       </div>  
       );
   }
