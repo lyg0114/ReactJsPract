53fc6f6f3f0bb0dfbba7c237c77d1725cab3ba9d
lyg0114 <whdnseowkd@naver.com>
2017-10-14 12:27:42 +0900

    #6-1. Ajax in React
    api 불러오기

--- movie_app/movie_app/src/App.js
+++ movie_app/movie_app/src/App.js
@@ -4,47 +4,15 @@ import Movie from './Movie';
 
 
 class App extends Component {
+  
   state = {
     
-    greeting:'Hello!',
-    testforstate:'stategggg',
-    
-    
+
   }
   
   componentDidMount(){
     
-    setTimeout( ()=>{
-      
-      this.setState({
-        
-        movies: [ //컴포넌트 내부로 데이터 이동
-          
-            { 
-              title: "Matrix2" ,
-              poster: "http://cfile2.uf.tistory.com/image/03217F4A509D113526C558" 
-            },
-    
-            { 
-              title: "Full Metal Jacket",
-              poster: "https://resizing.flixster.com/LTuNtJsmLevuXO2kS_Uh8xMUDa0=/206x305/v1.bTsxMTE2ODAyOTtqOzE3NTQ0OzEyMDA7ODAwOzEyMDA",//자켓
-            },
-    
-            {
-              
-              title: "Oldboy" ,
-              poster: "https://upload.wikimedia.org/wikipedia/ko/thumb/4/48/Old_Boy.jpg/250px-Old_Boy.jpg" 
-            },
-    
-            {
-              title: "Star Wars" ,
-              poster: "http://cfile29.uf.tistory.com/image/275C6D465613CFDD0D4D4A" 
-            }
-    
-          ]
-          })
-
-    },1000)
+    fetch('https://yts.ag/api/v2/list_movies.json?sort_by')
     
   }
 
