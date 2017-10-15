75be7cd5238dca8dfc9c775f9be503babe41618f
lyg0114 <whdnseowkd@naver.com>
2017-10-15 19:25:46 +0900

    #6-3. Async Await in React
    api 불러오기 다듬기!

--- movie_app/movie_app/src/App.js
+++ movie_app/movie_app/src/App.js
@@ -5,25 +5,34 @@ import Movie from './Movie';
 
 class App extends Component {
   
-  state = {
-    
-
-  }
+  state = {}
   
   componentDidMount(){
+    this._getMovies();
     
-    fetch('https://yts.ag/api/v2/list_movies.json?sort_by=like_count')
-    .then(response => response.json())
-    .then(json => console.log(json))
-    .catch(err => console.log(err))
   }
-
   _renderMovies = () =>{
-    const movies = this.state.movies.map( (movie,index) =>{ 
-      return <Movie title={movie.title} poster={movie.poster} key={index} />
+    const movies = this.state.movies.map(movie =>{ 
+      return <Movie title={movie.title} poster={movie.large_cover_image} key={movie.id} />
     })
     return movies
   }
+
+  _getMovies = async () => {
+    
+    const movies = await this._callApi()
+    this.setState({
+        movies
+      })
+  }
+
+  _callApi = () =>{
+    
+    return fetch('https://yts.ag/api/v2/list_movies.json?sort_by=like_count')
+    .then(response => response.json())
+    .then(json => json.data.movies)
+    .catch(err => console.log(err))
+}
 
   render() { 
     return ( 
