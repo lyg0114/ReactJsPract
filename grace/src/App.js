f7b1629472c524e3f6cb744c038e2087488ad3c3
lyg0114 <whdnseowkd@naver.com>
2017-10-25 20:05:55 +0900

    Taking Turns
    O,X 컨트롤

--- grace/src/App.js
+++ grace/src/App.js
@@ -2,44 +2,57 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class Square extends Component {
+/*
+class Square extends Component { //controlled components.
 
   render(){
     return(
       <button className="square" 
       onClick={() => this.props.onClick()}>
-      
       {this.props.value}
-      
       </button>
     )
   }
 
 }
+*/
+
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
+}
+
 
 class App extends Component {
   
   constructor(props){ //생성자
     super(props);
-    this.state = {
-      squares: Array(9).fill(null), //9개의 비어있는 배열
+    this.state = { //stat를 정의해둠
+      squares1: Array(9).fill(null), //9개의 비어있는 배열
+      xIsNext: true,
+
     }
   }
   
   handleClick(i) {
-    const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({squares: squares});
+    const squares2 = this.state.squares1.slice(); //state를 복사해서 사용!!
+    squares2[i] =  this.state.xIsNext ? 'X' : 'O'; //squares2[i] 
+    this.setState({squares1: squares2 ,
+                   xIsNext: !this.state.xIsNext,
+    }); //바뀐 부분만 업데이트 해준다!!
   }
 
   renderSquare(i){
-    return <Square value={this.state.squares[i]}
-                  onClick={() => this.handleClick(i)}
-    />;  
+    return <Square value={this.state.squares1[i]} onClick={() => this.handleClick(i)} />;  
   }
+ 
   render() {
     
-    const status = 'Next player: X';
+    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    
     return (
       
     <div>
